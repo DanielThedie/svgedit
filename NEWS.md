919cc20b004cbff2cf387d2798c1eab27ef40bda
DanielThedie <dthedie@gmail.com>
2025-11-28 12:09:59 +0000

    Initial commit

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# svgedit (development version)
+
+* Initial CRAN submission.
---
+++ R/draw.R
@@ -0,0 +1,4 @@
+#' Replace an svg object by a ggplot2 graph
+draw <- function(input_svg, output_svg, plots, dpi=150) {
+    
+}
